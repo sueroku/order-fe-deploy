ab59cb9c1372937b548dc45eb0150b23283db96e
sueroku <eraltndusn@gmail.com>
2024-08-23 14:05:57 +0900

    8th

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -10,7 +10,7 @@
                     </div>
                 </v-col>
                 <v-col class="text-center">
-                    <v-btn :to="{path:'/'}" class="text-h4">🦝suguri best store🦝</v-btn>
+                    <v-btn :to="{path:'/'}" class="text-h4">🦝suguri store🦝</v-btn>
                 </v-col>
                 <v-col class="d-flex justify-end">
                     <v-btn v-if="isLogin" :to="{path:'/order/cart'}">장바구니({{ getTotalQuantity }})</v-btn>
